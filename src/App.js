40453afaeac57c921143f219402463aeeaff34a2
Thomas Fouquert <tfouquert@gmail.com>
2022-11-22 19:48:29 +0100

    basic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import ScrollToTop from './util/ScrollToTop'
+import Home from './components/Home';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
 
-    </div>
+      <div>
+          <BrowserRouter>
+            <ScrollToTop >
+                <Navigation />
+                  <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                  </Routes>
+              </ScrollToTop>
+          </BrowserRouter>
+      </div>
+
+
+
   );
 }
 
