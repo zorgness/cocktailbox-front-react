29c0cda6141e518e49c0fb84a1e579466d0f25b7
Thomas Fouquert <tfouquert@gmail.com>
2022-11-25 18:31:39 +0100

    push on heroku

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -18,29 +18,28 @@ const Navigation = ({authData, logout, handleSearch}) => {
     <Navbar className="navbar-custom" expand="lg">
       <Container fluid>
         <Navbar.Brand href="/">CocktailBox</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" className='text-white' />
+        <Navbar.Toggle aria-controls="navbarScroll" />
       </Container>
 
       <Container>
-        <Navbar.Offcanvas id="navbarScroll" >
+        <Navbar.Collapse id="navbarScroll">
           <Nav
-
+            className=""
             navbarScroll
 
-
           >
-            <Nav.Link className="mx-3 text-white" href="/">Home</Nav.Link>
+            <Nav.Link className="mx-3" href="/">Home</Nav.Link>
             {
               authData.isAuthenticated
               ?
                 <Fragment>
-                  <Nav.Link href={`/favorites/${userId}`} className='mx-3 text-white'>Favoris</Nav.Link>
-                  <Nav.Link className='mx-3 text-white' onClick={handleLogout}>Logout</Nav.Link>
+                  <Nav.Link href={`/favorites/${userId}`} className='mx-3'>Favoris</Nav.Link>
+                  <Nav.Link className='mx-3' onClick={handleLogout}>Logout</Nav.Link>
                 </Fragment>
               :
                 <Fragment>
-                  <Nav.Link href="/register" className='mx-3 text-white'>Register</Nav.Link>
-                  <Nav.Link href="/login" className='mx-3 text-white'>Login</Nav.Link>
+                  <Nav.Link href="/register" className='mx-3'>Register</Nav.Link>
+                  <Nav.Link href="/login" className='mx-3'>Login</Nav.Link>
                 </Fragment>
             }
 
@@ -52,7 +51,7 @@ const Navigation = ({authData, logout, handleSearch}) => {
 
 
 
-        </Navbar.Offcanvas>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
